6d56d1ea224a8bf240a0f27850edc9ef9959924e
Andres <andres.jaag@hotmail.es>
2022-06-03 17:11:14 -0500

    Lleida_Sodig until create excel without dateAndre

--- Lleida_Sodig/src/main/java/com/mycompany/lleida_sodig/SodigLleida.java
+++ Lleida_Sodig/src/main/java/com/mycompany/lleida_sodig/SodigLleida.java
@@ -32,9 +32,11 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  * @author Andrés Aymacaña
  */
 public class SodigLleida {
+      static String mail_id, mail_date,fecha_andre, mail_type, file_doc_model, file_uid, unidades_certificadas="1.00", mail_from, mail_to,direccion_CC="correo@certificado.lleida.net", gstatus, gstatus_aux, mail_subj, add_id, add_displaydate, add_uid;
+
      public static void main(String[] args) throws MalformedURLException, IOException {
-        System.out.println("Hola mundo");
-          int contador=0;
+        System.out.println("Espere");
+          int contador=0,numeroCelda=0;
         // Creamos el libro de trabajo de Excel formato OOXML
         Workbook book=new XSSFWorkbook();
          // La hoja donde pondremos los datos
@@ -43,13 +45,26 @@ public class SodigLleida {
         Row row=sheet.createRow(contador);
         row.createCell(0).setCellValue("Id");
         row.createCell(1).setCellValue("Fecha Lleida");
+        row.createCell(2).setCellValue("Tipo");
+        row.createCell(3).setCellValue("Doc_OkKo");
+        row.createCell(4).setCellValue("Doc_UID");
+        row.createCell(5).setCellValue("Unidades Certificadas");
+        row.createCell(6).setCellValue("Dirección Origen");
+        row.createCell(7).setCellValue("Dirección Destino");
+        row.createCell(8).setCellValue("Dirección Cc");
+        row.createCell(9).setCellValue("Estado");
+        row.createCell(10).setCellValue("Estado Aux");
+        row.createCell(11).setCellValue("Asunto");
+        row.createCell(12).setCellValue("Doc_Visualizado");
+        row.createCell(13).setCellValue("Fecha y hora de visualización");
+        row.createCell(14).setCellValue("Add_UID");
         
         
         
-        try {
+       try {
          
-        // URL url= new URL("https://tsa.lleida.net/cgi-bin/mailcertapi.cgi?action=list_pdf&user=sodigsa@ec&password=TIiANcmymJ&mail_id=100274269");
-       URL url= new URL("https://tsa.lleida.net/cgi-bin/mailcertapi.cgi?action=list_pdf&user=sodigsa@ec&password=TIiANcmymJ&mail_date_min=20220501070000&mail_date_max=20220601070000");
+        //URL url= new URL("https://tsa.lleida.net/cgi-bin/mailcertapi.cgi?action=list_pdf&user=sodigsa@ec&password=TIiANcmymJ&mail_id=97277518");
+        URL url= new URL("https://tsa.lleida.net/cgi-bin/mailcertapi.cgi?action=list_pdf&user=sodigsa@ec&password=TIiANcmymJ&mail_date_min=20220501070000&mail_date_max=20220601070000");
         HttpsURLConnection conn=(HttpsURLConnection)url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
@@ -67,25 +82,171 @@ public class SodigLleida {
                     if (linea.contains("<mail_id>")) {
                         int tamano=linea.length();
                         int fin=tamano-10;
-                        contador ++;
-                        row=sheet.createRow(contador);
-                        row.createCell(0).setCellValue(linea.substring(9, fin));
+                        mail_id=linea.substring(9, fin);
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_id: "+mail_id);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_date>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-12;
+                        mail_date=linea.substring(17,19)+"/"+linea.substring(15,17)+"/"+linea.substring(11,15)+" "+linea.substring(19,21)+":"+linea.substring(21,23)+":"+linea.substring(23,25);
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_date: "+mail_date);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_type>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-12;
+                        mail_type=linea.substring(11, fin);
                         
                         //System.out.println(linea.length());
                         //System.out.println(linea);
                         //System.out.println("mail_id: "+linea.substring(9, fin));
-                        informationString.append("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_date: "+mail_type);
                         informationString.append("\n");
-                    }else if(linea.contains("<mail_date>")) {
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<file_doc_model>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-17;
+                        file_doc_model=linea.substring(16, fin);
+                        
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("file_doc_model: "+file_doc_model);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<file_uid>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-11;
+                        file_uid=linea.substring(10, fin);
+                                               
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("file_uid: "+file_uid);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_from>")) {
                         int tamano=linea.length();
                         int fin=tamano-12;
-                        row.createCell(1).setCellValue(linea.substring(11, fin));
+                        mail_from=linea.substring(11, fin);
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_from: "+mail_from);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_to>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-10;
+                        mail_to=linea.substring(9, fin);
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_to: "+mail_to);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<gstatus>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-10;
+                        gstatus=linea.substring(9, fin);
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("gstatus: "+gstatus);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<gstatus_aux>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-14;
+                        gstatus_aux=linea.substring(13, fin);
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("gstatus_aux: "+gstatus_aux);
+                        informationString.append("\n");
+                    }else if(linea.contains("<gstatus_aux/>")) {
+                        gstatus_aux="";
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("gstatus_aux: "+gstatus_aux);                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_subj>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-12;
+                        mail_subj=linea.substring(11, fin);
+                         //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_subj: "+mail_subj);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<add_id>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-9;
+                        add_id=linea.substring(8, fin);
+                        add_uid = "E" + add_id + "-R";
+                        add_id = "Displayed";                        
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("add_id: "+add_id);
+                        informationString.append("\n");
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<add_displaydate>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-18;
+                        add_displaydate=linea.substring(23,25)+"/"+linea.substring(21,23)+"/"+linea.substring(17,21)+" "+linea.substring(25,27)+":"+linea.substring(27,29)+":"+linea.substring(29,31);
+                        
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("add_displaydate: "+add_displaydate);
+                        informationString.append("\n");
+                    }/*
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_date>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-12;
+                        row.createCell(numeroCelda).setCellValue(linea.substring(11, fin));
+                        numeroCelda++;
                         //System.out.println(linea.length());
                         //System.out.println(linea);
                         //System.out.println("mail_id: "+linea.substring(9, fin));
                         informationString.append("mail_date: "+linea.substring(11, fin));
                         informationString.append("\n");
-                    }else if(linea.contains("</pdf_row>")) {
+                    }
+                    //+++++++++++++++++++++++++++++
+                    else if(linea.contains("<mail_date>")) {
+                        int tamano=linea.length();
+                        int fin=tamano-12;
+                        row.createCell(numeroCelda).setCellValue(linea.substring(11, fin));
+                        numeroCelda++;
+                        //System.out.println(linea.length());
+                        //System.out.println(linea);
+                        //System.out.println("mail_id: "+linea.substring(9, fin));
+                        informationString.append("mail_date: "+linea.substring(11, fin));
+                        informationString.append("\n");
+                    }*/
+                    else if(linea.contains("</pdf_row>")) {
                         int tamano=linea.length();
                         int fin=tamano-10;
                         //System.out.println(linea.length());
@@ -93,6 +254,44 @@ public class SodigLleida {
                         //System.out.println("mail_id: "+linea.substring(9, fin));
                        // informationString.append("gstatus: "+linea.substring(9, fin));
                         informationString.append("\n");
+                        contador++;
+                        row=sheet.createRow(contador);
+                         
+                        row.createCell(numeroCelda).setCellValue(mail_id);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(mail_date);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(mail_type);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(file_doc_model);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(file_uid);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(unidades_certificadas);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(mail_from);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(mail_to);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(direccion_CC);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(gstatus);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(gstatus_aux);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(mail_subj);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(add_id);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(add_displaydate);
+                        numeroCelda++;
+                        row.createCell(numeroCelda).setCellValue(add_uid);
+                        numeroCelda++;
+                        
+                        //seteo de variables
+                        mail_date=mail_date=mail_type=file_doc_model=file_uid=mail_from=mail_to=gstatus=gstatus_aux=mail_subj=add_id=add_uid=add_displaydate="";
+                        //nueva fila
+                        numeroCelda=0;
                     }
                                        
                 }
